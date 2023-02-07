a0ac46bbbb7d98f91edebcb9794156f86f67d390
Sturgart <wesleyicaro4@gmail.com>
2023-02-06 23:19:11 -0300

    Quarto commit

--- movies_house/src/pages/Movie.jsx
+++ movies_house/src/pages/Movie.jsx
@@ -24,6 +24,13 @@ const Movie = () => {
 
     setMovie(data);
   }
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
   
   useEffect(() => {
     const movieURL = `${moviesURL}${id}?${apiKey}`
@@ -31,7 +38,37 @@ const Movie = () => {
   }, [])
 
   return <div>
-    {movie && <>{movie.original_title}</>}
+    {movie && (
+
+      <>
+      <MovieCard movie={movie} showLink={false} />
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Orçamento:
+          </h3>
+          <p>{formatCurrency(movie.budget)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsGraphUp /> Receita:
+          </h3>
+          <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duração:
+          </h3>
+          <p>{movie.runtime} minutos</p>
+        </div>
+        <div className="info description">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Descrição:
+          </h3>
+          <p>{movie.overview}</p>
+        </div>
+    </>
+    )}
   </div>;
 };
 
